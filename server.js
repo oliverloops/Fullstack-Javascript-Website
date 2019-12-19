2be63cd2bdb46423d330086e94bb45f3c4df60d5
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2019-12-18 23:58:34 -0600

    Hey, Hey!

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ const express = require("express"),
     path = require("path"),
     bodyParser = require("body-parser"),
     handlebars = require("handlebars"),
-    expressHandlebars = require('express-handlebars');
+    expressHandlebars = require('express-handlebars'),
+    MongoClient = require("mongodb").MongoClient,
+    assert = require('assert');
 
 const app = express();
 const port = process.env.PORT || 5050;
 
+// DB SETTINGS
+const url = 'mongodb://oliverhuxley:Quince0698.@ds243049.mlab.com:43049/asterdots';
+let db;
+const dbName = 'asterdots';
+
+const dbCon = MongoClient.connect(url, db, (err, client) => {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    db = client.db(dbName);
+  });
+
+// MIDDLEWARE CONFIG.
 app.use(bodyParser.urlencoded({ 
     extended: true 
 }));
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public/assets'));
 
@@ -27,9 +41,10 @@ app.get('/' ,(req, res) => {
 });
 
 //FORM ENTRY -- AUTH ERRORS
-app.post('/registro', (req, res) => {
-    console.log(req.body);
-    res.render('./home');
+app.post('/registro', async (req, res) => {
+    delete req.body._id;
+    let collection = await db.collection('users').insertOne(req.body);
+    res.render("./home");
 });
 
 //ERROR 500
